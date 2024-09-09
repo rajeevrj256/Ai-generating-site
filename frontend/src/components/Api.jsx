19f508739dba038941960bbb75c2c225582d60f3
rajeevrj256 <rjrajeev5918@gmail.com>
2024-09-10 01:30:06 +0530

    status code

--- frontend/src/components/Api.jsx
+++ frontend/src/components/Api.jsx
@@ -24,7 +24,7 @@ export const fetchDataFromAPI = async (aiInput, userInput, setfetchdata) => {
 
     const data = await response.json();
     //console.log("response",data.response);
-    if(data.response && data.response.status===429){
+    if(data.response && data.response.status===500){
       setfetchdata("Too Many Request");
       return;
     }
